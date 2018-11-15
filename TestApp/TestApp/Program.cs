94baa586d2f95e4772616c218ba09a38f1092f88
Nilsson82 <Anders858@gmail.com>
2018-11-15 21:28:44 +0100

    Print Anders

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -6,7 +6,8 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 9;i++)
+              Console.WriteLine("Hello Anders!\n");
         }
     }
 }
